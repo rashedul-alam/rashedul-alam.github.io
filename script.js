64b29682d4102dc9b438e990e42558ddca3b43bd
rashedul-alam <ra_shed@hotmail.com>
2020-10-31 02:33:12 +0600

    version 3

---
+++ script.js
@@ -0,0 +1,34 @@
+let theme = localStorage.getItem('theme');
+
+
+let themeDots = document.getElementsByClassName('theme-dot');
+if (theme == null) {
+    setTheme('light');
+} else {
+    setTheme(theme);
+}
+
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function () {
+        let mode = this.dataset.mode;
+        console.log('option clicked');
+        setTheme(mode);
+    })
+
+}
+
+function setTheme(mode) {
+    if (mode == 'light') {
+        document.getElementById('theme-style').href = 'main.css';
+    }
+    if (mode == 'blue') {
+        document.getElementById('theme-style').href = 'blue.css';
+    }
+    if (mode == 'green') {
+        document.getElementById('theme-style').href = 'green.css';
+    }
+    if (mode == 'purple') {
+        document.getElementById('theme-style').href = 'purple.css';
+    }
+    localStorage.setItem('theme', mode);
+};
